3411230a3cb41f6f57d09d8bbf542de0acb76e78
Marko Taali <marko.taali@bang.fi>
2017-05-21 14:20:00 +0300

    Removed unnecessary console.logs

--- flix.js
+++ flix.js
@@ -1,31 +1,26 @@
 window.onload = function() {
-
-    console.log('hey hoe.');
-
-    // Get Api Key from Local Storage
+    // Get Api Key from Local Storage and place it in the input
     getMyApiKey( function(value) {
         document.getElementById("apikey").value = value.myapikey;
     });
-
-    console.log('finished.');
-
 };
 
-function getMyApiKey(callback) {
+// Function for asynchronous request
+function getMyApiKey(callback) {  
     chrome.storage.local.get('myapikey', callback);
 }
 
+// Put new key into global storage
 function setApiKey() {
-
-    console.log('New Key!');
-
     var newApiKey = document.getElementById("apikey").value;
     chrome.storage.local.set({'myapikey': newApiKey});
-
+    // Let the user know that the new value was saved
     document.getElementById('messages').innerHTML = "New key set: "+newApiKey;    
 }
 
+// Handle clicks
 function _clickHandler() {
+    // If there's a new value, set it
     if (document.getElementById('apikey').value.length < 1 || document.getElementById('apikey').value == 'Enter your API key') {
         return false;
     } else {
